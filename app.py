c1de7cdac2a56fd0f9294c84a14d1c2c0e4316d3
Kevin Deakers <kmdeakers@mac.com>
2023-03-20 15:35:27 -0700

    working on patch

--- app.py
+++ app.py
@@ -59,3 +59,27 @@ def create_cupcake():
 
     # Return w/status code 201 --- return tuple (json, status)
     return (jsonify(cupcake=serialized), 201)
+
+@app.patch("/api/cupcakes/<int:cupcake_id>")
+def update_cupcake(cupcake_id):
+    """Update cupcake information from posted JSON and return it
+    Expects JSON: {flavor, size, rating, image (optional)}
+    Return JSON: {'cupcake': {id, flavor, size, rating, image}}"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    data = request.json.copy()
+    keys = request.json.keys()
+    print ('data:', data)
+    print ('keys:', keys)
+    print ('cupcake:', cupcake)
+    for key in keys:
+        cupcake[key] = request.json[key]
+
+    db.session.commit()
+
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized) 
+
+
